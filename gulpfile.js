80ba50d2ef3d18c5d5f9ba504507195ec8705540
eduard <webeduard@yandex.ru>
2017-02-24 23:05:44 +0300

    Added sprites, delete jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp         = require('gulp'),
     rename       = require("gulp-rename"),
-		sass         = require('gulp-sass'),
-		autoprefixer = require('gulp-autoprefixer'),
-		cleanCSS     = require('gulp-clean-css'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS     = require('gulp-clean-css'),
     sourcemaps   = require('gulp-sourcemaps'),
     cached       = require('gulp-cached'),
     csscomb      = require('gulp-csscomb'),
-		rename       = require('gulp-rename'),
+    rename       = require('gulp-rename'),
     plumber      = require('gulp-plumber'),
-		browserSync  = require('browser-sync').create(),
-		concat       = require('gulp-concat'),
-		uglify       = require('gulp-uglify'),
+    browserSync  = require('browser-sync').create(),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
     cheerio      = require('gulp-cheerio'),
     svgmin       = require('gulp-svgmin'),
     svgSymbols   = require('gulp-svg-symbols'),
@@ -80,8 +80,7 @@ gulp.task('styles', function () {
               'Safari >= 9.1'
         ]
     ))
-	// .pipe(cleanCSS())
-  .pipe(csscomb())
+	.pipe(cleanCSS())
   .pipe(sourcemaps.write())
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.stream());
@@ -89,13 +88,10 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function() {
 	return gulp.src([
-		'./app/libs/modernizr/modernizr.js',
-		'./app/libs/jquery/jquery-1.11.2.min.js',
     './app/libs/swiper/swiper.jquery.min.js',
     './app/libs/scrollMagic/ScrollMagic.min.js',
     './app/libs/greensock/TweenMax.min.js',
-    './app/libs/scrollMagic/animation.gsap.min.js',
-    './app/libs/popup/jquery.magnific-popup.min.js'
+    './app/libs/scrollMagic/animation.gsap.min.js'
 	])
 	.pipe(concat('libs.js'))
 	.pipe(uglify())
